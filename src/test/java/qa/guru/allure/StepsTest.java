0a5bf4b441dce542624d922c26838b901adee0fe
Elena Sokolova <>
2023-07-08 19:25:29 +0200

    added StepsTest

---
+++ src/test/java/qa/guru/allure/StepsTest.java
@@ -0,0 +1,38 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+
+public class StepsTest extends TestBase {
+
+    private static final String REPOSITORY = "lena-sokolova/hw11_allure";
+
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open(baseUrl);
+        });
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(".header-search-button").click();
+            $("#query-builder-test").setValue(REPOSITORY);
+            $("#query-builder-test").submit();
+        });
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Проверяем наличие таба Issue в " + REPOSITORY, () -> {
+            $("#issues-tab").shouldBe(Condition.visible);
+        });
+    }
+}
